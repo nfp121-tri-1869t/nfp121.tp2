17b70bb816b02d176372d5e63dabc38fb475414a
Kasabaki <omar.kasabaki@isae.edu.lb>
2019-03-22 22:10:44 +0200

    q1

--- tp2/question1/FahrenheitCelsius.java
+++ tp2/question1/FahrenheitCelsius.java
@@ -16,11 +16,14 @@ public class FahrenheitCelsius {
 	 *            ...
 	 */
 	public static void main(String[] args) {
+	    int fahrenheit = 0;
+	    float celsius = 0;
 		// pour tous les paramètres de la ligne de commande
-		int fahrenheit = 0;
-		float celsius = 0;
+		for(int i =0; i<args.length;i++){
+		fahrenheit = Integer .parseInt(args[i]);
+		celsius = fahrenheitEnCelsius(fahrenheit);
 		System.out.println(fahrenheit + "\u00B0F -> " + celsius + "\u00B0C"); // ligne,
-																				// format
+}																		// format
 																				// imposés
 																				
 	}
@@ -34,7 +37,8 @@ public class FahrenheitCelsius {
 	 */
 	public static float fahrenheitEnCelsius(int f) {
 
-		return 0.F; // à compléter en remplaçant ce return 0.F par la fonction
+	float c = (float)5/9*(f-32);
+        return ((int)(c*10)/10.0F); // à compléter en remplaçant ce return 0.F par la fonction
 					// de conversion
 	}
 
